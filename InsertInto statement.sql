b2350cb73152ffba56db0a8208c04eb326621440
Ofentse Malebye <119665605+OfentseLearnsCode@users.noreply.github.com>
2023-05-02 16:25:12 +0200

    Add files via upload

---
+++ InsertInto statement.sql
@@ -0,0 +1,16 @@
+
+insert into employee (employee_number, first_name, last_name, email_address)
+values (0005,'Ofentse', 'Malebye', 'malebyeoc@gmail.com')
+
+insert into employee (employee_number, first_name, last_name, email_address)
+values (0004,'Lebogang', 'Mokoena', 'mokoenalebogang@gmail.com')
+
+insert into employee (employee_number, first_name, last_name, email_address)
+values (0003,'Rolva', 'Mashale', 'mashaler@gmail.com')
+
+
+insert into employee (employee_number, first_name, last_name, email_address)
+values (0002,'Refiloe', 'Setshedi', 'refiloes@gmail.com')
+
+insert into employee (employee_number, first_name, last_name, email_address)
+values (0001,'Lethabo', 'Makhafola', 'lethabom@gmail.com')
