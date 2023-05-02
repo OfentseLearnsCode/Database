762edab7a6c0f0f6100546dc09fc367f900aef25
Ofentse Malebye <119665605+OfentseLearnsCode@users.noreply.github.com>
2023-05-02 16:24:17 +0200

    Add files via upload

---
+++ UpdateStatement.sql
@@ -0,0 +1,7 @@
+UPDATE employee 
+SET first_name  = value1, last_name= value2, email_Address = value3
+WHERE condition;
+
+UPDATE department 
+SET first_name  = value1, last_name= value2, email_Address = value3
+WHERE condition;
